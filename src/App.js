b6330e5a395a42a98bd73cddf5ed3080fdf3f15f
Thomas BENARD <thomas0benard@gmail.com>
2021-08-10 11:39:37 +0200

    created header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="container">
+      <div className="col-md-6 mx-auto">
+        <h3 className="text-center my-5">Admin Password</h3>
+      </div>
+    </div>
+  );
 }
 
 export default App;
