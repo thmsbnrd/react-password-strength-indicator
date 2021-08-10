b642b9c97acecb05ce4d45ba13cd75c783fb0a02
Thomas BENARD <thomas0benard@gmail.com>
2021-08-10 20:10:41 +0200

    changed progress bar colors

--- src/components/PasswordStrengthMeter.js
+++ src/components/PasswordStrengthMeter.js
@@ -5,9 +5,28 @@ const PasswordStrengthMeter = ({ password }) => {
   const testResult = zxcvbn(password);
   const num = (testResult.score * 100) / 4;
 
+  const strengthColor = (num) => {
+    switch (num) {
+      case 50:
+        return "#FFD166";
+        break;
+      case 75:
+        return "#06D6A0";
+        break;
+      case 100:
+        return "#06D6A0";
+        break;
+      default:
+        return "#EF476F";
+        break;
+    }
+  };
+
+  console.log(strengthColor(num));
+
   const changePasswordColor = () => ({
     width: `${num}%`,
-    background: "red",
+    background: `${strengthColor(num)}`,
     height: "7px",
   });
 
