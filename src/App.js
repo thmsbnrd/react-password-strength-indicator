3cc90d227e6197f66b9618b2cca577ccd4be1ea3
Thomas BENARD <thomas0benard@gmail.com>
2021-08-12 12:04:12 +0200

    aicons to toggle password show

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import PasswordStrengthMeter from "./components/PasswordStrengthMeter";
 import "./App.css";
+import { ReactComponent as VisibilityOn } from "./icons/visibility_black_24dp.svg";
+import { ReactComponent as VisibilityOff } from "./icons/visibility_off_black_24dp.svg";
 
 function App() {
   const [password, setPassword] = useState("");
@@ -17,14 +19,21 @@ function App() {
             placeholder="mot de passe"
             onChange={(e) => setPassword(e.target.value)}
           />
-          <input
-            className="toggle-password"
-            type="checkbox"
-            onClick={() => {
-              setPasswordVisibility(!passwordVisibility);
-              console.log(passwordVisibility);
-            }}
-          />
+          {passwordVisibility ? (
+            <VisibilityOn
+              className="toggle-password"
+              onClick={() => {
+                setPasswordVisibility(!passwordVisibility);
+              }}
+            />
+          ) : (
+            <VisibilityOff
+              className="toggle-password"
+              onClick={() => {
+                setPasswordVisibility(!passwordVisibility);
+              }}
+            />
+          )}
         </div>
         <PasswordStrengthMeter password={password} />
       </div>
